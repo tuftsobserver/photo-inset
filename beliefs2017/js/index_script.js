d322701598618a518d4e0618ee73f38e62fd240c
Deanna Baris <deanna.baris@gmail.com>
2017-03-03 19:25:46 -0500

    working on carousel

--- beliefs2017/js/index_script.js
+++ beliefs2017/js/index_script.js
@@ -4,15 +4,19 @@
 // });
 
 var mySiema = new Siema({
-    selector: '.mySiema',
+    selector: '.siema',
     duration: 200,
     easing: 'ease-out',
     perPage: 3,
     startIndex: 0,
-    draggable: true,
+    draggable: false,
     threshold: 20,
     loop: true,
 });
 
 document.querySelector('.js-prev').addEventListener('click', function() {mySiema.prev()});
 document.querySelector('.js-next').addEventListener('click', function() {mySiema.next()});
+
+// for (var i = 0; i < howManySlides) {
+// 	if (i != currentSlide)
+// }
