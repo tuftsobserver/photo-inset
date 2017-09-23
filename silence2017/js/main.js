ce735064d977a34931771f60b96b9723ece3f2e2
Peter Lam <Peter.Lam@Tufts.edu>
2017-09-23 14:08:00 -0400

    Images styled

--- silence2017/js/main.js
+++ silence2017/js/main.js
@@ -1,5 +1,23 @@
 $(function() {
 	$.scrollify({
-		section: ".scroll"
+		section: ".scroll",
 	});
-})
+});
+
+$('.arrow').click(function() {
+	$.scrollify.move('#2');
+});
+
+$(document).ready(function() {
+	$('h1').fadeOut( 2000, function() {
+		$('h1').css("color", "white");
+		$('h1').css("text-shadow", "5px 5px 15px #736c7a");
+	    $('h1').fadeIn(1000);
+	});
+});
+
+$('docume').scroll(function() {
+	$('h1').fadeOut( "slow", function() {
+
+	});
+});
